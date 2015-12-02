e0d5c0f605df704e92b4cd03cebd0062a90581fd
Peilin Duan <duan@g.harvard.edu>
2015-12-02 00:18:59 -0500

    still problemetic

--- FillingPixels.cl
+++ FillingPixels.cl
@@ -17,30 +17,59 @@ int get_index(int w, int h, int x, int y){
 __kernel void
 FillingPixels(__global float * gpu_Image,
               __global __read_only float * gpu_texture,
-              __global __read_only float * gpu_imfilled,
+              __global float * gpu_imfilled,
               __global __read_only float * gpu_Gaussian,
-              __local float *workgroup,
-              __global __write_only float * outImage,
+              __local float * workgroup,
+              __local float * mask,
+              //__global __write_only float * outImage,
               __global int * I, //Locations of pixels to fill
-              __global int * J,
+              __global int * J, //Locations of pixels to fill
               int width, int height,
-              int buf_w, int buf_h,
-              int window_size)
+              int tex_w, int tex_h,
+              int window_size, float MaxErr)
 {
     int k=get_global_id(2);
     // get center
-    int c_row=I[k];
-    int c_col=J[k];
+    int cy=I[k];
+    int cx=J[k];
+    //printf("center %i %i", cx,cy);
     // get upper left corner
-    int cor_row=c_row-(window_size-1)/2;
-    int cor_col=c_col-(window_size-1)/2;
+    int cor_x=cx-(window_size-1)/2;
+    int cor_y=cy-(window_size-1)/2;
     // filling local work group -- template
-    for(int row=0;i<window_size;i++)
-        for(int col=0;j<window_size;j++)
+    for(int y=0;y<window_size;y++)
+    {
+        for(int x=0;x<window_size;x++)
         {
-            workgroup[row*window_size+col]=gpu_Image(get_index(width,height,cor_col+col,cor_row+row));
+            workgroup[y*window_size+x]=gpu_Image[get_index(width,height,cor_x+x,cor_y+y)];
+            mask[y*window_size+x]=gpu_imfilled[get_index(width,height,cor_x+x,cor_y+y)]*gpu_Gaussian[y*window_size+x];
         }
-        
-
-
+    }
+    // Find Match
+    float bestErr=100;
+    float bestValue=0;
+    for(int oy=0;oy<tex_h-window_size+1;oy++)
+    {
+        for(int ox=0;oy<tex_w-window_size+1;ox++)
+        {
+            float err=0;
+            for(int y=0;y<window_size;y++)
+            {
+                for(int x=0;x<window_size;x++)
+                {
+                    err+=mask[y*window_size+x]*(workgroup[y*window_size+x]-gpu_texture[(y+oy)*tex_w+x+ox])*(workgroup[y*window_size+x]-gpu_texture[(y+oy)*tex_w+x+ox]);
+                }
+            }
+            if(err<bestErr)
+            {
+                bestErr=err;
+                bestValue=gpu_texture[(oy+(window_size-1)/2)*tex_w+ox+(window_size-1)/2];
+            }
+        }
+    }
+    //if(bestErr<MaxErr)
+    //{
+        gpu_Image[get_index(width,height,cx,cy)]=bestValue;
+        gpu_imfilled[get_index(width,height,cx,cy)]=1;
+    //}
 }
--- OpenCL_TextureSyn.py
+++ OpenCL_TextureSyn.py
@@ -2,10 +2,10 @@
 import numpy as np
 import math
 import scipy
-#from FindMatches import *
 from scipy import ndimage
 import pyopencl as cl
 import os.path
+import pylab
 
 
 if __name__ == '__main__':
@@ -39,31 +39,17 @@ if __name__ == '__main__':
     print 'The queue is using the device:', queue.device.name
 
     curdir = os.path.dirname(os.path.realpath(__file__))
-    program = cl.Program(context, open('FindMatches.cl').read()).build(options=['-I', curdir])
+    program = cl.Program(context, open('FillingPixels.cl').read()).build(options=['-I', curdir])
 
-    host_texture = ndimage.imread('rings.jpg').astype(np.float32)[::2, ::2].copy()
-    host_Synth_Image=np.zeros([100,100])
-    synthdim=[100,100]
 
-    gpu_texture = cl.Buffer(context, cl.mem_flags.READ_WRITE, host_texture.size * 4)
-   
+    host_texture = ndimage.imread('rings.jpg').astype(np.float32)
+
     # template window size
     w = 13
-    halo = (w-1)/2
-    global_size = (host_texture.shape[0],host_texture.shape[1])
-    local_size = (global_size[0]-2*halo,global_size[1]-2*halo)
-    width = np.int32(host_texture.shape[1])
-    height = np.int32(host_texture.shape[0])
-    # Since the texture is very small, we read it all in to GPU
-    local_memory = cl.LocalMemory(4*global_size[0]*global_size[1])
-    buf_width = np.int32(global_size[1])
-    buf_height = np.int32(global_size[0])
-
-    gpu_validmask = cl.Buffer(context, cl.mem_flags.READ_WRITE, w*w*4)
-    gpu_Gaussian = cl.Buffer(context, cl.mem_flags.READ_WRITE, w*w*4)
-
-    cl.enqueue_copy(queue, gpu_texture, host_texture, is_blocking=False)
-
+    synthdim=[100,100]
+    tex_width = np.int32(host_texture.shape[1])
+    tex_height = np.int32(host_texture.shape[0])
+    
     MaxErrThreshold=0.3
     sigma=w/6.4
     x,y=np.meshgrid(range(-(w-1)/2,(w-1)/2+1),range(-(w-1)/2,(w-1)/2+1))
@@ -71,51 +57,79 @@ if __name__ == '__main__':
     nfilled=0
     tofill=synthdim[0]*synthdim[1]-9 # the number of pixels to fill
 
-    cl.enqueue_copy(queue, gpu_Gaussian, G, is_blocking=False)
+    synthim=(np.ones((synthdim[0],synthdim[1]))*(-1)).astype(np.float32)
+    im_filled=np.zeros((synthdim[0],synthdim[1])).astype(np.float32) # image for testing the original image is filled or not
 
-    synthim=np.ones((synthdim[0],synthdim[1]))*(-1)
-    im_filled=np.zeros((synthdim[0],synthdim[1])) # image for testing the original image is filled or not
-    offset=(w-1)/2  # offset for zero padding image
     # place the seed in the center and zero padding
-    synthim[(math.floor(synthdim[0]/2)-1):(math.floor(synthdim[0])/2+1), (math.floor(synthdim[1]/2)-1):(math.floor(synthdim[1]/2)+1)]=host_texture[5:7,4:6]
-    synthim_padded=np.lib.pad(synthim,(((w-1)/2, (w-1)/2),((w-1)/2, (w-1)/2)),'constant', constant_values=0)
+    synthim[(math.floor(synthdim[0]/2)-1):(math.floor(synthdim[0])/2+2), (math.floor(synthdim[1]/2)-1):(math.floor(synthdim[1]/2)+2)]=host_texture[5:8,4:7]
+ 
+    # GPU Global Buffers
+    gpu_imfilled = cl.Buffer(context, cl.mem_flags.READ_WRITE, synthdim[0]*synthdim[1]*4)
+    gpu_texture = cl.Buffer(context, cl.mem_flags.READ_WRITE, host_texture.size * 4)
+    gpu_Gaussian = cl.Buffer(context, cl.mem_flags.READ_WRITE, w*w*4)
+    gpu_Image = cl.Buffer(context, cl.mem_flags.READ_WRITE, synthim.size * 4)
+
+
+    cl.enqueue_copy(queue, gpu_texture, host_texture, is_blocking=False)
+    cl.enqueue_copy(queue, gpu_Image, synthim, is_blocking=False)
+    cl.enqueue_copy(queue, gpu_Gaussian, G, is_blocking=False)
+    
     # find unfilled neighbors
     se= ndimage.generate_binary_structure(2,2)  # use a 3 by 3 structuring element for dilation
     im_filled=(synthim>=0)
     im_dil=ndimage.binary_dilation(im_filled,structure=se)
     [I,J]=np.nonzero(im_dil-im_filled)
+    gpu_I = cl.Buffer(context, cl.mem_flags.READ_WRITE, len(I)*8)
+    gpu_J = cl.Buffer(context, cl.mem_flags.READ_ONLY, len(I)*8)
 
 
-    match_list=cl.Buffer(context, cl.mem_flags.READ_WRITE, w*4)
+    cl.enqueue_copy(queue, gpu_imfilled, im_filled, is_blocking=False)
+    cl.enqueue_copy(queue, gpu_I, I, is_blocking=False)
+    cl.enqueue_copy(queue, gpu_J, J, is_blocking=False)
 
-    while nfilled<tofill:
-        progress=0;
-            # get template
-        for i in range(0,len(I)):
-                template=synthim_padded[I[i]+offset-(w-1)/2:I[i]+offset+(w-1)/2+1,J[i]+offset-(w-1)/2:J[i]+offset+(w-1)/2+1]
-                validmask=(template>=0)
-                cl.enqueue_copy(queue, gpu_validmask, validmask, is_blocking=False)
-                #match_list = FindMatches(template, validmask, sourceimage, G)
-                program.FindMatches(queue, global_size, local_size, 
-                                    gpu_texture, gpu_validmask, gpu_Gaussian, template,
-                                    local_memory, 
-                                    match_list,
-                                    width, height,
-                                    buf_width, buf_height, halo)
-                print match_list
-                BstInd=np.random.randint(0,len(match_list))
-                BestMatch=match_list[BstInd][1]
-                MatchErr=match_list[BstInd][0]
-                if MatchErr<MaxErrThreshold:
-                    synthim_padded[I[i]+offset,J[i]+offset]=BestMatch
-                    progress=1
-                    nfilled=nfilled+1
-                    im_filled[I[i],J[i]]=1
-        if progress==0:
-                MaxErrThreshold=MaxErrThreshold*1.1
-        im_dil=ndimage.binary_dilation(im_filled,structure=se)
-        [I,J]=np.nonzero(im_dil-im_filled)
+
+    # GPU Local work group
+    workgroup = cl.LocalMemory(4*w*w)
+    mask = cl.LocalMemory(4*w*w)
+
+    # global size and local size
+    global_size = (1,1,len(I))
+    local_size = (1,1,1)
+    im_filled1=np.zeros([100,100])
+    pylab.imshow(im_filled1)
+    pylab.show()
+    for i in range(len(I)):
+        program.FillingPixels(queue, global_size, local_size,
+                              gpu_Image, gpu_texture, gpu_imfilled, gpu_Gaussian,
+                              workgroup, mask, gpu_I, gpu_J, 
+                              np.int32(synthdim[0]),np.int32(synthdim[1]),np.int32(tex_width),np.int32(tex_height),
+                              np.int32(w),np.float32(MaxErrThreshold))
+    
+
+    cl.enqueue_copy(queue, im_filled1, gpu_imfilled, is_blocking=True)
+
+    pylab.imshow(im_filled1)
+    pylab.show()
+
+    print sum(sum(im_filled))
+
+
+    
+    #print 'done!'
+
+
+    # while nfilled<tofill:
+    #     progress=0;
+    #     # Call Kernel
+    #     program.FillingPixels(queue, global_size, local_size,
+    #                           gpu_Image, gpu_texture, gpu_imfilled, gpu_Gaussian,
+    #                           workgroup, mask, I, J, 
+    #                           synthdim[0],synthdim[1],tex_width, tex_height,w,MaxErrThreshold)
+    #     if progress==0:
+    #             MaxErrThreshold=MaxErrThreshold*1.1
+    #     im_dil=ndimage.binary_dilation(im_filled,structure=se)
+    #     [I,J]=np.nonzero(im_dil-im_filled)
         
-    synthim=synthim_padded[offset:offset+synthdim[0],offset:offset+synthdim[1]]
+    # synthim=synthim_padded[offset:offset+synthdim[0],offset:offset+synthdim[1]]
 
 
