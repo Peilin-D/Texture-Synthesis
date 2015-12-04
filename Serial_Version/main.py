958c4f24887fd272906f3792c34b7b7ddb31a79e
Peilin Duan <duan@g.harvard.edu>
2015-12-04 15:46:17 -0500

    little modifications

--- Serial_Version/main.py
+++ Serial_Version/main.py
@@ -1,14 +1,15 @@
 import numpy as np 
 from scipy import ndimage
 import pylab
+from PIL import Image
 from SynthTexture import *
 
 if __name__ == "__main__":
 
-	tex=ndimage.imread('rings.jpg')
+	tex=ndimage.imread('../textures/rings.jpg')
 	tex=tex/255.0;
 	#print tex.shape[0], tex.shape[1]
 	Sythim=SynthTexture(tex, 13, [50,50])
 
-	pylab.imshow(Sythim)
-	pylab.show()
+	im_out=Image.fromarray(Sythim*255)
+	im_out.show()
