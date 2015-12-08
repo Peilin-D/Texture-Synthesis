fbc7ec082b1e55d40d4e47bb983316ab62551624
Zelong Qiu <zelong430@gmail.com>
2015-12-08 18:11:25 -0500

    argument passing done

--- Serial_Version/main.py
+++ Serial_Version/main.py
@@ -4,42 +4,22 @@ import pylab
 from PIL import Image
 from SynthTexture import *
 import time
-
+import sys
 
 if __name__ == "__main__":
     
-    t0=time.time()
-    tex=ndimage.imread('../textures/rings.jpg')
+    #read input arguments
+    inputfile = sys.argv[1]
+    outputfile = sys.argv[2]
+    syn_size = int(sys.argv[3])
+
+    t0 = time.time()
+    tex=ndimage.imread(inputfile)
     tex=tex/255.0;
-	#print tex.shape[0], tex.shape[1]
-    Sythim=SynthTexture(tex, 15, [200,200])
+    Sythim=SynthTexture(tex, 15, [syn_size,syn_size])
     print "Serial Version ", time.time()-t0, " seconds"
     im_out=Image.fromarray(Sythim*255)
-    im_out.show()
+    im_out.convert('RGB').save(outputfile,"JPEG")
 
-    # t0=time.time()
-    # tex=ndimage.imread('../textures/D3.gif')
-    # tex=tex/255.0;
-    # #print tex.shape[0], tex.shape[1]
-    # Sythim=SynthTexture(tex, 15, [200,200])
-    # print "Serial Version ", time.time()-t0, " seconds"
-    # im_out=Image.fromarray(Sythim*255)
-    # im_out.show()
 
-    # t0=time.time()
-    # tex=ndimage.imread('../textures/D20.gif')
-    # tex=tex/255.0;
-    # #print tex.shape[0], tex.shape[1]
-    # Sythim=SynthTexture(tex, 15, [200,200])
-    # print "Serial Version ", time.time()-t0, " seconds"
-    # im_out=Image.fromarray(Sythim*255)
-    # im_out.show()
-
-    # t0=time.time()
-    # tex=ndimage.imread('../textures/161.gif')
-    # tex=tex/255.0;
-    # #print tex.shape[0], tex.shape[1]
-    # Sythim=SynthTexture(tex, 15, [200,200])
-    # print "Serial Version ", time.time()-t0, " seconds"
-    # im_out=Image.fromarray(Sythim*255)
-    # im_out.show()
+   
